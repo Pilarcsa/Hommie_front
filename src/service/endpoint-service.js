cecaf8a87513d97bf12b75c21b276adfbe98989d
pilarcsa <pilarcsa.99@gmail.com>
2026-04-08 12:54:23 +0200

    endpoint onrender

--- src/service/endpoint-service.js
+++ src/service/endpoint-service.js
@@ -6,7 +6,7 @@ const points = {
     "x2": "https://hommie.onrender.com/api"
 }
 
-const BASE_URL = import.meta.env.VITE_API_BASE || points.x1 // Base URL actual para las peticiones
+const BASE_URL = import.meta.env.VITE_API_BASE || points.x2 // Base URL actual para las peticiones
 
 // Definición de todos los endpoints de la API
 export const endPoints = {
